138213efc6571f4fa6f53a85faa47297b562f10a
9names <60134748+9names@users.noreply.github.com>
2020-08-24 00:55:26 +1000

    Passing tests, good enough

---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![feature(new_uninit)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+extern "C" fn callback(_pdraw: *mut JPEGDRAW) {
+    
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+    #[test]
+    fn decode() {
+        // Need to let Rust trust us with some initialized memory to store our C data
+        let image = Box::<JPEGIMAGE>::new_uninit();
+        let image = unsafe { image.assume_init() };
+        let imgptr: *mut JPEGIMAGE = Box::into_raw(image);
+        
+        // include_bytes gives us an immutable slice, copy that into a mutable one
+        let tulips_const = include_bytes!("tulips.jpg");
+        let tulips: &mut [u8] = &mut tulips_const.clone();
+        let tulipsmut = tulips.as_mut_ptr();
+
+        // Must wrap our callback function
+        let c: JPEG_DRAW_CALLBACK = Some(callback);
+        unsafe {
+            let opened = JPEG_openRAM(imgptr, tulipsmut, tulips.len() as i32, c);
+            assert!(opened != 0, "Could not open file, {}", JPEG_getLastError(imgptr));
+            if opened != 0 {
+                let rc = JPEG_decode(imgptr, 0, 0, 0);
+                let errstr = JPEG_getLastError(imgptr);
+                JPEG_close(imgptr);
+                assert!(rc == 1, "Could not decode file, {}", errstr);
+            }     
+        }
+    }
+}
