bdd5ae03aa75d9462d9cd5ae5315b5dbfe5271c4
9names <60134748+9names@users.noreply.github.com>
2020-08-24 19:21:42 +1000

    Ran code through Cargo::fmt

--- src/lib.rs
+++ src/lib.rs
@@ -18,9 +18,9 @@ mod tests {
         // Need to let Rust trust us with some initialized memory to store our C data
         // We can do that by getting C to zero-init a struct and return it
         // Obviously that's unsafe, so wrap that in an unsafe block
-        let image = Box::new(unsafe{JPEG_ZeroInitJPEGIMAGE()});
+        let image = Box::new(unsafe { JPEG_ZeroInitJPEGIMAGE() });
         let imgptr: *mut JPEGIMAGE = Box::into_raw(image);
-        
+
         // include_bytes gives us an immutable slice, copy that into a mutable one
         let tulips_const = include_bytes!("tulips.jpg");
         let tulips: &mut [u8] = &mut tulips_const.clone();
@@ -30,13 +30,17 @@ mod tests {
         let c: JPEG_DRAW_CALLBACK = Some(callback);
         unsafe {
             let opened = JPEG_openRAM(imgptr, tulipsmut, tulips.len() as i32, c);
-            assert!(opened != 0, "Could not open file, {}", JPEG_getLastError(imgptr));
+            assert!(
+                opened != 0,
+                "Could not open file, {}",
+                JPEG_getLastError(imgptr)
+            );
             if opened != 0 {
                 let rc = JPEG_decode(imgptr, 0, 0, 0);
                 let errstr = JPEG_getLastError(imgptr);
                 JPEG_close(imgptr);
                 assert!(rc == 1, "Could not decode file, {}", errstr);
-            }     
+            }
         }
     }
 }
